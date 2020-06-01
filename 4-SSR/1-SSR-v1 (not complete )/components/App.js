92dab52624fa337e9e80e9f5204f45cd51697db3
Abi John <37790773+switch2aby@users.noreply.github.com>
2020-06-01 20:36:41 +0400

    Add files via upload

---
+++ 4-SSR/1-SSR-v1 (not complete )/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                im am App Component
+            </div>
+        );
+    }
+}
+
+export default App;
